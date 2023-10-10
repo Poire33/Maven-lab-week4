76e6a2396bbc8b6552959e02448bd716ffe96260
Rupert spelled with one P <g00423530@atu.ie>
2023-10-10 14:09:00 +0100

    Added a new method called multiply

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -6,6 +6,7 @@ public class Calc {
     public static void main(String[] args) {
         add();
         subtract();
+        multiply();
     }
 
     public static void add()
@@ -31,6 +32,19 @@ public class Calc {
         int secondNumber = inputs.nextInt();
 
         int total = firstNumber - secondNumber;
+        System.out.println("The total is " + total);
+    }
+
+    public static void multiply()
+    {
+        System.out.println("Please enter your first number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter your second number: ");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber * secondNumber;
         System.out.println("The total is " + total);
     }
 }
